4ed0245346d9a924e25ab18ed33894d76b81ba98
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-21 09:29:30 -0400

    Room Chat works

--- app.js
+++ app.js
@@ -12,19 +12,29 @@ const server = app.listen(port, () => {
 
 const io = require('socket.io')(server);
 
-var messages = [];
+// var messages = [];
 io.on('connection', socket => {
-    socket.emit("message", messages);
+    // socket.emit("message", messages);
     console.log("Client connected with id: " + socket.id);
+
+    socket.on("join-room", room => {
+        socket.join(room);
+        console.log("User " + socket.id + " connected to room " + room);
+    });
+
     socket.on("message", (data) => {
+        const room = data[2];
+        console.log("sala: " + room);
+
         const message = {
             text: data[0],
             senderId: data[1]
         };
 
-        messages.push(message);
-        console.log(messages);
-        io.emit("message", messages);
+        // messages.push(message);
+        // console.log(messages);
+        console.log("message: " + message.text);
+        socket.to(room).emit("message", message);
         // socket.to(room).emit("message", message);
     });
 });
